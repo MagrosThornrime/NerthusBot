7cbf2477c910a831717c5c85dbb838d96041582e
MagrosThornrime <magros.thornrime@gmail.com>
2024-11-06 00:47:04 +0100

    Add skill points field for player input

--- declarations.py
+++ declarations.py
@@ -1,5 +1,6 @@
 from datetime import date
 import re
+import random
 
 import discord as dc
 from discord.ext.modal_paginator import ModalPaginator, PaginatorModal, CustomButton
@@ -41,39 +42,22 @@ class DeclarationPaginator(ModalPaginator):
         "FINISH": FinishButton()
     }
 
-    def parse_points(self, points: str) -> str:
-        points_description = "- PU:\n"
-        pattern = r"^\"(.*)\"[ \t]*(0\.[0-9]+)$"
-        for line in points.splitlines():
-            match_object = re.fullmatch(pattern, line)
-            if match_object is None:
-                raise ValueError((
-                    "**Nie udało się wysłać deklarki**\n\n"
-                    "Każda linijka w liście graczy powinna składać "
-                    "się z nazwy postaci "
-                    "wziętej w cudzysłowie (np \"Brimm Schadenfreude\"), "
-                    "odstępu, a potem liczby PU. "
-                    "Liczba PU powinna być zapisana jako ułamek dziesiętny "
-                    "większa od 0 i mniejsza od 1.0, np 0.45.\n\n"
-                    "Przykład poprawnej linijki:\n"
-                    "\"Brimm Schadenfreude\" 0.45"
-                ))
-            nickname = match_object[1]
-            points_number = match_object[2]
-            points_description += f"\t- {nickname}: {points_number}\n"
-        return points_description
+    def parse_players(self, players: list[tuple[str]]) -> str:
+        players_description = "- PU:\n"
+        for player, points in players:
+            players_description += f"\t- {player}: {points}\n"
+        return players_description
 
     def create_declaration(self, name: str, places: str, logs: str,
-                           description: str, points: str) -> str:
+                           description: str, players: list[tuple[str]]) -> str:
         current_date = date.today().isoformat()
-        # points_description = self.parse_points(points)
-        points_description = "\n".join([f"\t- {nick}" for nick in points])
+        players_description = self.parse_players(players)
         declaration = ("```md\n"
                        f"### {current_date}, {name}\n"
                        f"*Lokalizacje: {places}*\n\n"
                        f"Logi: {logs}\n"
                        f"> {description}\n\n"
-                       f"{points_description}\n"
+                       f"{players_description}\n"
                        "```"
                        )
         return declaration
@@ -99,7 +83,7 @@ class DeclarationPaginator(ModalPaginator):
             label="Opis",
             style=dc.TextStyle.long,
             placeholder=(
-                "Anward zwyzywał wory z Fortu"
+                "Anward zwyzywał wory z Fortu "
                 "oraz popodglądał sparing Rose i Brimm."
             ),
             max_length=1400
@@ -107,11 +91,18 @@ class DeclarationPaginator(ModalPaginator):
         return modal
 
     def create_players_modal(self, first_player: int, rows: int) -> PaginatorModal:
-        modal = PaginatorModal(title="Dodaj graczy")
+        modal = PaginatorModal(title=f"Dodaj graczy - część {first_player // 2 + 1}")
         for row in range(rows):
+            player_number = first_player + rows + 1
             modal.add_input(
-                label=f"Gracz nr {first_player + row}",
-                max_length=30
+                label=f"Nick gracza nr. {player_number}",
+                max_length=30,
+                placeholder=random.choice(("Velrose", "Anward", "Brimm Schadenfreude"))
+            )
+            modal.add_input(
+                label=f"Sugerowana liczba PU dla gracza nr. {player_number}",
+                max_length=5,
+                placeholder=str(round(random.random(), 2))
             )
         return modal
 
@@ -122,8 +113,8 @@ class DeclarationPaginator(ModalPaginator):
         if players > 16:
             raise ValueError("Ale że aż tyle graczy?")
         self.add_modal(self.create_main_modal())
-        for i in range(0, players, 5):
-            modal = self.create_players_modal(i, min(players - i, 5))
+        for i in range(0, players, 2):
+            modal = self.create_players_modal(i, min(players - i, 2))
             self.add_modal(modal)
 
     async def on_finish(self, interaction: dc.Interaction) -> None:
@@ -134,8 +125,13 @@ class DeclarationPaginator(ModalPaginator):
         logs = main_modal.children[2].value
         description = main_modal.children[3].value
         for modal in self.modals[1:]:
-            for field in modal.children:
-                players.append(field.value)
+            name = ""
+            for index, field in enumerate(modal.children):
+                if index % 2 == 0:
+                    name = field.value
+                else:
+                    points = field.value
+                    players.append((name, points))
         declaration = self.create_declaration(name, places, logs,
                                               description, players)
         await interaction.response.send_message(declaration)
