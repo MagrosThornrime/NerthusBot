08fc0a6b73684f97dee33e2408078863435fd82d
MagrosThornrime <magros.thornrime@gmail.com>
2024-11-05 21:34:02 +0100

    Migrate to discord.py instead of using py-cord

--- bot.py
+++ bot.py
@@ -9,12 +9,11 @@ import discord as dc
 load_dotenv()
 
 intents = dc.Intents().default()
-intents.message_content = True
 testing_guild = 868754530639171594
-bot = dc.Bot(debug_guilds=[testing_guild,], intents=intents)
+client = dc.Client(debug_guilds=[testing_guild,], intents=intents)
+tree = dc.app_commands.CommandTree(client)
 
-class DeclariationModal(dc.ui.Modal):
-    
+class DeclarationModal(dc.ui.Modal, title="Uzupełnij deklarkę"):
     def parse_points(self, points: str) -> str:
         points_description = "- PU:\n"
         pattern = r"^\"(.*)\"[ \t]*(0\.[0-9]+)$"
@@ -36,15 +35,13 @@ class DeclariationModal(dc.ui.Modal):
             points_number = match_object[2]
             points_description += f"\t- {nickname}: {points_number}\n"
         return points_description
-            
-        
     
-    def create_declaration(self, title: str, places: str, logs: str,
+    def create_declaration(self, name: str, places: str, logs: str,
                            description: str, points: str) -> str:
         current_date = date.today().isoformat()
         points_description = self.parse_points(points)
         declaration = ("```md\n"
-                       f"### {current_date}, {title}\n"
+                       f"### {current_date}, {name}\n"
                        f"*Lokalizacje: {places}*\n\n"
                        f"Logi: {logs}\n"
                        f"> {description}\n\n"
@@ -52,64 +49,57 @@ class DeclariationModal(dc.ui.Modal):
                        "```"
         )
         return declaration
-
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
         
-        title = dc.ui.InputText(label="Tytuł deklarki",
-                                placeholder="Zwykły dzień w Forcie Eder",
-                                max_length=100)
-        places = dc.ui.InputText(label="Lokalizacje",
-                                 placeholder="Fort Eder, Fortyfikacja p.1",
-                                 max_length=100)
-        logs = dc.ui.InputText(label="Link do logów",
-                               placeholder="najlepiej skorzystać z pastebina",
-                               max_length=100)
-        description = dc.ui.InputText(label="Opis",
-                                      style=dc.InputTextStyle.long,
-                                      placeholder=(
-                                        "Anward zwyzywał wory z Fortu"
-                                        "oraz popodglądał sparing Rose i Brimm."
-                                        ),
-                                      max_length=1550)
-        points = dc.ui.InputText(label=(
-                                        "Lista graczy i sugestia"
-                                        "ile powinni dostać PU"
-                                        ),
-                                 style=dc.InputTextStyle.long,
-                                 placeholder=(
-                                            "\"Anward\" 0.2\n"
-                                            "\"Velrose\" 0.3\n"
-                                            "\"Brimm Schadenfreude\" 0.3"
-                                            ),
-                                 max_length=350)
-        for input_text in (title, places, logs, description, points):
-            self.add_item(input_text)
+    name = dc.ui.TextInput(label="Tytuł deklarki",
+                            placeholder="Zwykły dzień w Forcie Eder",
+                            max_length=100)
+    places = dc.ui.TextInput(label="Lokalizacje",
+                             placeholder="Fort Eder, Fortyfikacja p.1",
+                             max_length=100)
+    logs = dc.ui.TextInput(label="Link do logów",
+                           placeholder="najlepiej skorzystać z pastebina",
+                           max_length=100)
+    description = dc.ui.TextInput(label="Opis",
+                                  style=dc.TextStyle.long,
+                                  placeholder=(
+                                    "Anward zwyzywał wory z Fortu"
+                                    "oraz popodglądał sparing Rose i Brimm."
+                                    ),
+                                  max_length=1550)
+    points = dc.ui.TextInput(label=(
+                                "Lista graczy i sugestia"
+                                "ile powinni dostać PU"
+                                ),
+                             style=dc.TextStyle.long,
+                             placeholder=(
+                                        "\"Anward\" 0.2\n"
+                                        "\"Velrose\" 0.3\n"
+                                        "\"Brimm Schadenfreude\" 0.3"))
 
-    async def callback(self, interaction: dc.Interaction):
-        title = self.children[0].value
-        places = self.children[1].value
-        logs = self.children[2].value
-        description = self.children[3].value
-        points = self.children[4].value
-        try:
-            declaration = self.create_declaration(title, places, logs,
-                                                  description, points)
-        except ValueError as e:
-            await interaction.response.send_message(str(e), ephemeral=True)
-            return
+    async def on_submit(self, interaction: dc.Interaction) -> None:
+        declaration = self.create_declaration(self.name.value,
+                                              self.places.value,
+                                              self.logs.value,
+                                              self.description.value,
+                                              self.points.value)
         await interaction.response.send_message(declaration)
 
-@bot.event
+    async def on_error(self, interaction: dc.Interaction,
+                       error: Exception) -> None:
+        await interaction.response.send_message(str(error), ephemeral=True)
+
+@client.event
 async def on_ready():
-    print(f"{bot.user} is ready and online!")
+    await tree.sync()
+    print(f"{client.user} is ready and online!")
 
-    
-@bot.slash_command()
-async def send_declaration(ctx: dc.ApplicationContext):
-    """Stwórz i wyślij deklarkę. Trafi na ten kanał i specjalny kanał Rady."""
-    modal = DeclariationModal(title="Uzupełnij deklarkę")
-    await ctx.send_modal(modal)
+@tree.command(
+    name="send_declaration",
+    description="Stwórz i wyślij deklarkę. Trafi na ten kanał i specjalny kanał Rady."
+)
+async def send_declaration(interaction: dc.Interaction):
+    modal = DeclarationModal()
+    await interaction.response.send_modal(modal)
 
-bot.run(os.getenv('TOKEN'))
+
+client.run(os.getenv('TOKEN'))
